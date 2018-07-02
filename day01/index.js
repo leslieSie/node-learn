9afc36d67e3a4955fa26310546716849386db4fc
“cae <xiecanyong@zskuaixiao.com>
2018-07-02 10:24:20 +0800

    update

--- day01/index.js
+++ day01/index.js
@@ -1,8 +1,8 @@
 var CountStream = require('./countStream');
-var countStream = new CountStream('book');
+var countStream = new CountStream('Node');
 let http = require('http');
 
-http.get('http://www.manning.com', (res) => {
+http.get('http://www.runoob.com/nodejs/nodejs-stream.html', (res) => {
     res.pipe(countStream);
 });
 
